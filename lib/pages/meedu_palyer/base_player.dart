fae282ed6abc8df6e50b9d2eb63237da15f3e802
dgbkn <anandrambkn@gmail.com>
2023-07-25 18:31:36 +0530

    meedu player is non good

--- lib/pages/meedu_palyer/base_player.dart
+++ lib/pages/meedu_palyer/base_player.dart
@@ -1,53 +1,53 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_meedu_videoplayer/meedu_player.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_meedu_videoplayer/meedu_player.dart';
 
-class MeeduPlayer extends StatefulWidget {
-  final url;
-  final name;
-  MeeduPlayer({this.name, this.url});
+// class MeeduPlayer extends StatefulWidget {
+//   final url;
+//   final name;
+//   MeeduPlayer({this.name, this.url});
 
-  @override
-  State<MeeduPlayer> createState() => _MeeduPlayerState();
-}
+//   @override
+//   State<MeeduPlayer> createState() => _MeeduPlayerState();
+// }
 
-class _MeeduPlayerState extends State<MeeduPlayer> {
-  final _meeduPlayerController = MeeduPlayerController();
+// class _MeeduPlayerState extends State<MeeduPlayer> {
+//   final _meeduPlayerController = MeeduPlayerController();
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void initState() {
+//     super.initState();
 
-    _meeduPlayerController.setDataSource(
-      DataSource(
-        type: DataSourceType.network,
-        source: widget.url,
-      ),
-      autoplay: true,
-    );
+//     _meeduPlayerController.setDataSource(
+//       DataSource(
+//         type: DataSourceType.network,
+//         source: widget.url,
+//       ),
+//       autoplay: true,
+//     );
   
-  }
+//   }
 
-  @override
-  void dispose() {
-    _meeduPlayerController.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _meeduPlayerController.dispose();
+//     super.dispose();
+//   }
 
   
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(  
-          child: AspectRatio(
-            aspectRatio: 16 / 9,
-            child: MeeduVideoPlayer(
-              controller: _meeduPlayerController,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Center(  
+//           child: AspectRatio(
+//             aspectRatio: 16 / 9,
+//             child: MeeduVideoPlayer(
+//               controller: _meeduPlayerController,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,32 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <dart_vlc/dart_vlc_plugin.h>
-#include <flutter_meedu_videoplayer/flutter_meedu_videoplayer_plugin_c_api.h>
-#include <fullscreen_window/fullscreen_window_plugin_c_api.h>
-#include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
-#include <media_kit_video/media_kit_video_plugin_c_api.h>
-#include <screen_brightness_windows/screen_brightness_windows_plugin.h>
-#include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DartVlcPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DartVlcPlugin"));
-  FlutterMeeduVideoplayerPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterMeeduVideoplayerPluginCApi"));
-  FullscreenWindowPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FullscreenWindowPluginCApi"));
-  MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
-  MediaKitVideoPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
-  ScreenBrightnessWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenBrightnessWindowsPlugin"));
-  ScreenRetrieverPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  WindowManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
